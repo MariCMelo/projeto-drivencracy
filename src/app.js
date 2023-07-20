98d55b7c029c93ca8ca3ff490c3ca0622359a9d5
MariCMelo <marilia.correa.melo@gmail.com>
2023-07-20 16:04:29 -0300

    Deploy config OK

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(router);
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
