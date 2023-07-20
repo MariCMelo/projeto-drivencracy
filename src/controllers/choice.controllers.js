12c5abed366d0a9069aa6496b965e24842481b15
MariCMelo <marilia.correa.melo@gmail.com>
2023-07-19 21:14:58 -0300

    post /choice working

--- src/controllers/choice.controllers.js
+++ src/controllers/choice.controllers.js
@@ -5,43 +5,42 @@ export async function createChoice(req, res) {
     const { title, pollId } = req.body;
 
     try {
-        const validationResult = schema.validate({ title, pollId });
-
-        if (validationResult.error) {
-            return res.status(422)
-        }
 
         const poll = await db
             .collection("polls")
-            .findOne({ _id: ObjectId(pollId) });
+            .findOne({ _id: new ObjectId(pollId) });
 
         if (!poll) {
-            return res
-                .status(404)
+            return res.status(404).json({ error: "Enquete não encontrada." });
         }
 
-        if (poll.expireAt < new Date()) {
-            return res.status(403)
+        if (!title) {
+            return res.status(422).json({ error: "O título não pode ser vazio." });
         }
+
         const existingChoice = await db
             .collection("choices")
             .findOne({ pollId, title });
+
         if (existingChoice) {
-            return res.status(409)
+            return res.status(409).json({ error: "Essa opção de voto já existe na enquete." });
+        }
+
+        if (poll.expireAt < new Date()) {
+            return res.status(403).json({ error: "A enquete já expirou." });
         }
 
         const choice = {
             title,
-            pollId
+            pollId,
         };
 
-        await db
-            .collection("choices")
-            .insertOne(choice);
+        await db.collection("choices").insertOne(choice);
 
-        res.status(201).send(choice);
+        res.status(201).json(choice);
+
     } catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send(err.message);
     }
 }
 
